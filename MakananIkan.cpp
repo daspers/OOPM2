fe217b1a9700a081139f6c1e1a5d7d402d7e80cd
AgeMagi <32269979+AgeMagi@users.noreply.github.com>
2018-04-08 17:12:30 +0700

    Update MakananIkan.cpp

--- MakananIkan.cpp
+++ MakananIkan.cpp
@@ -1,13 +1,36 @@
 #include "MakananIkan.hpp"
-#include "oop.hpp"
+
+int MakananIkan::banyakmakananikan = 0;
 
 //Ctor
-MakananIkan::MakananIkan(int x) : BendaAkuarium(x, 0, ARAH_MAKANAN_IKAN, KEC_MAKANAN_IKAN){}
+MakananIkan::MakananIkan(int x) : BendaAkuarium(x, 0, 0, 500) {
+	MakananIkan::banyakmakananikan++;
+	image = "makananikan";
+}
 
 
 void MakananIkan::gerak(){ //Update attribut ketika makanan ikan bergerak
-	int ty = get
-	if(y<=SCREEN_HEIGHT){
-		y += speed*sec_since_last;
+	if(abs(this->getY() - SCREEN_HEIGHT) > 0.1){
+		this->setY(this->getY() + 5);
 	}
+}
+
+void MakananIkan::setImage(string x) {
+	image = x;
+}
+
+string MakananIkan::getImage() {
+	return image;
+}
+
+int MakananIkan::getBanyakMakananIkan() {
+	return MakananIkan::banyakmakananikan;
+}
+
+bool MakananIkan::operator==(const MakananIkan& other) {
+	return (abs(this->getX() - other.getX()) < 0.1 && abs(this->getY() - other.getY()) < 0.1);
+}
+
+bool MakananIkan::operator!=(const MakananIkan& other) {
+	return (abs(this->getX() - other.getX()) > 0.1 || abs(this->getY() - other.getY()) > 0.1);
 }
