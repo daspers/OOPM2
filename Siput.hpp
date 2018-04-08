7c7bc042b75d62ba89fbb0e6182399bf43c8cfe1
yasyars <yasya.rusyda@gmail.com>
2018-04-09 00:18:37 +0700

    update Siput.hpp

--- Siput.hpp
+++ Siput.hpp
@@ -4,24 +4,32 @@
 #include "BendaAkuarium.hpp"
 #include "List.hpp"
 #include "Koin.hpp"
+#include <cstring>
 
+using namespace std;
 class Siput : public BendaAkuarium {
 //Kelas siput ialah turunan dari kelas Benda Akuarium yang merupakan benda yang
 //mengambil koin koin yang ada pada akuarium
 	private :
-	  	const int radiuskoin; //Radius apakah koin dapat diambil atau tidak
-	  	Posisi PointTujuan;
+	  	Posisi pointtujuan;
+      string image;
   public :
+    //Ctor 
+    Siput(Posisi);
+    //Cctor
+    Siput (const Siput&);
+    //Operator overloading =
+    Siput& operator=(const Siput&);
+
   	//getter 
-  	Posisi getPointTujuan(); //Mengembalikan nilai point tujuan
-  	int getRadiusKoin(); //Mengembalikan nilai Radius Koin dari siput
-
-  	//setter
+  	Posisi getPointTujuan() const; //Mengembalikan nilai point tujuan
+    string getImage() const;
+  	
+    //setter
   	void setPointTujuan(Posisi); //Mengubah nilai Point Tujuan dari Siput
-
+    void setImage(string);
   	void gerak(); //Prosedur untuk pergerakan dari iput
-	Posisi cariKoinTerdekat(List<Koin>); //Untuk mencari koin terdekat dari posisi siput
-	void ambilKoin(Koin); //Untuk mengambil suatu koin yang sudan mendekati radius dari siput
-};
+  	Posisi cariKoinTerdekat(List<Koin>); //Untuk mencari koin terdekat dari posisi siput
+  };
 
 #endif
