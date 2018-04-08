f0fc40cf0ffb44d5eb4d479b8e6ebecc58b58608
AgeMagi <32269979+AgeMagi@users.noreply.github.com>
2018-04-08 17:13:39 +0700

    Update MakananIkan.hpp

--- MakananIkan.hpp
+++ MakananIkan.hpp
@@ -1,20 +1,38 @@
 #ifndef MAKANANIKAN_HPP
 #define MAKANANIKAN_HPP
 
+#include <iostream>
 #include "BendaAkuarium.hpp"
+#include <cstring>
+#include <cmath>
+#include "oop.hpp"
+
+using namespace std;
 
 #define ARAH_MAKANAN_IKAN 90
 #define KEC_MAKANAN_IKAN 10 
 
 class MakananIkan : public BendaAkuarium {
-	//Kelas Makanan Ikan ialah turunan dari kelas benda akuarium yang menyimpan 
-	//nilai nilai yang dimiliki oleh sebuah makanan ikan
-	private :
-		static int jumlahmakananikan; //Mengembalikan jumlah makanan ikan yang ada pada akuarium
-	public :
-		//Ctor
-		MakananIkan(int x);
-		void gerak(); //Update attribut ketika makanan ikan bergerak
+  //Kelas Makanan Ikan ialah turunan dari kelas benda akuarium yang menyimpan 
+  //nilai nilai yang dimiliki oleh sebuah makanan ikan
+  private :
+    static int banyakmakananikan; //Mengembalikan jumlah makanan ikan yang ada pada akuarium
+    string image;
+  public :
+    //Ctor
+    MakananIkan(int x);
+    
+    void gerak(); //Update attribut ketika makanan ikan bergerak
+
+    void setImage(string x);
+
+    bool operator==(const MakananIkan&);
+
+    bool operator!=(const MakananIkan&);
+
+    string getImage();
+
+    static int getBanyakMakananIkan();
 };
 
 #endif
