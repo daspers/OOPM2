435dd21771bf51b61cc32f533adb6d24fa53eddb
AgeMagi <32269979+AgeMagi@users.noreply.github.com>
2018-04-08 17:15:31 +0700

    Update Ikan.hpp

--- Ikan.hpp
+++ Ikan.hpp
@@ -1,88 +1,121 @@
-#ifndef IKAN_HPP
-#define IKAN_HPP
+#include "Ikan.hpp"
 
-#include <iostream>
-using namespace std;
+int Ikan::banyakikan = 0;
 
-#include "BendaAkuarium.hpp"
-#include "Koin.hpp"
-#include "oop.hpp"
-#include <math.h>
+Ikan::Ikan(double x, double y, double arah, double kecepatan) : BendaAkuarium(x, y, arah, kecepatan), tahankenyang(5), hunger(10), pointtujuan(rand()%SCREEN_WIDTH, rand()%SCREEN_HEIGHT) {
+	lapar = false;
+	waktumakan = 0;
+	banyakikan++;
+	image = "ikankiri.png";
+}
 
-//Kelas ikan adalah kelas yang merupakan derived kelas dari benda akuarium dan merupakan base kelas dari piranha dan guppy
-//Ikan sendiri punya atribut berupa koin yang akan di produksi waktu ikan tahan kenyang
-//serta waktu yang akan mempertahankan ikan hidup sampai mati ketika sudah masuk fase lapar
-//Juga memiliki atribut berupa waktumakan yang berrati menghitung setiap kali ikan makan untuk perubahan level dari ikan
-//serta type yang berisi string yang menyimpan type dari ikan sendiri
+Ikan::Ikan(const Ikan& other) : BendaAkuarium(other.getX(), other.getY(), other.getArah(), other.getKecepatan()), tahankenyang(5), hunger(10) {
+	this->lapar = other.getLapar();
+	this->waktumakan = other.getWaktuMakan();
+}
 
-class Ikan : public BendaAkuarium {
-	private :
-	  	// Koin produksi;
-	    bool lapar;
-	    const int tahankenyang; //Waktu untuk berapa lama dia kenyang
-	  	int waktumakan;
-	    const int hunger; //Waktu untuk menahan lapar
-	    string type;
-		// const int radiusmakanan; //Waktu 
-		Posisi pointtujuan;
-		string image;
+Ikan& Ikan::operator=(const Ikan& other) {
+	this->setX(other.getX());
+	this->setY(other.getY());
+	this->setArah(other.getArah());
+	this->setKecepatan(other.getKecepatan());
+}
 
-	public :
-	//ctor tanpa parameter dengan kondisi default yang akan didefenisikan kedepannya
-  	Ikan(double x, double y, double arah, double kecepatan);
+//cctor
+// Ikan::Ikan(const Ikan&);
 
-	//cctor
-    // Ikan(const Ikan&);
-	
-	//dtor
-    // ~Ikan();
-	
-	//operator overloading untuk assignment
-	Ikan& operator=(const Ikan&);
-	
-	//Mengembalikan koin yang diproduksi oleh ikan
-    // Koin getProduksi() const;
+//dtor
+// Ikan::~Ikan();
 
-    //Mengembalikan kondisi ikan lapar menjadi true else false
-	bool getLapar() const;
+//operator overloading untuk assignment
+// Ikan& Ikan::operator=(const Ikan&);
 
-	//Mengembalikan waktu yang dibutuhkan sampai mencapai kondisi harus memakan/lapar
-    int getTahanKenyang() const;
+//Mengembalikan koin yang diproduksi oleh ikan
+// Koin Ikan::getProduksi() const {
+// 	return this.produksi;
+// }
 
-	//Mengembalikan banyak kali ikan makan yang digunakan untuk mengupdate level ikan kedepannya
-    int getWaktuMakan() const;
-	
-	//Mengembalikan waktu yang dibutuhkan sampai mati karena kelaparan
-	int getHunger() const;
-	
-	//Mengembalikan nilai point tujuan sebuah ikan
-    Posisi getPointTujuan() const;
-	
-	//Mengembalikan type ikan apakah guppy atau piranha
-    string getType() const;
-    
-    string getImage() const;
+//Mengembalikan kondisi ikan lapar menjadi true else false
+bool Ikan::getLapar() const {
+	return this->lapar;
+}
 
-    //prosedur untuk mengubah produksi koin dari ikan
-    void setProduksi(Koin x);
-	
-	//prosedur untuk mengubah kondisi lapar dari ikan
-    void setLapar(bool x);
-	
-	//prosedur untuk mengubah waktu makan dari ikan
-    void setWaktuMakan(int x);
-    
-    //Prosedur untuk mengubah point tujuan dari ikan
-    void setPointTujuan(Posisi x);
-	
-    void setImage(string x);
+//Mengembalikan waktu yang dibutuhkan sampai mencapai kondisi harus memakan/lapar
+int Ikan::getTahanKenyang() const {
+	return this->tahankenyang;
+}
 
-  	//virtual method untuk gerak ikan
-	virtual void gerak();
-	
-	//virtual method untuk ikan makan
-	// virtual void makan();
-	
-};
+//Mengembalikan banyak kali ikan makan yang digunakan untuk mengupdate level ikan kedepannya
+int Ikan::getWaktuMakan() const {
+	return this->waktumakan;
+}
 
-#endif
+//Mengembalikan waktu yang dibutuhkan sampai mati karena kelaparan
+int Ikan::getHunger() const {
+	return this->hunger;
+}
+
+//Mengembalikan nilai point tujuan sebuah ikan
+Posisi Ikan::getPointTujuan() const {
+	return this->pointtujuan;
+}
+
+//Mengembalikan type ikan apakah guppy atau piranha
+string Ikan::getType() const {
+	return this->type;
+}
+
+string Ikan::getImage() const {
+    return image;
+}
+
+//prosedur untuk mengubah produksi koin dari ikan
+// void Ikan::setProduksi(Koin x) {
+// 	produksi = x;
+// }
+
+//prosedur untuk mengubah kondisi lapar dari ikan
+void Ikan::setLapar(bool x) {
+	lapar = x;
+}
+
+//prosedur untuk mengubah waktu makan dari ikan
+void Ikan::setWaktuMakan(int x) {
+	waktumakan = x;
+}
+
+//Prosedur untuk mengubah point tujuan dari ikan
+void Ikan::setPointTujuan(Posisi x) {
+	pointtujuan = x;
+}
+
+void Ikan::setImage(string x){
+    image = x;
+}
+
+//virtual method untuk gerak ikan
+void Ikan::gerak() {
+	if (!lapar) {
+	    this->setArah(atan2(pointtujuan.getY()-this->getY(), pointtujuan.getX()-this->getX()));
+	    if (this->getArah()*180/PI > -90 && this->getArah()*180/PI < 90) {
+            image = "ikankanan.png";
+        } else {
+            image = "ikankiri.png";
+        }
+        this->setX(this->getX() + this->getKecepatan()*cos(this->getArah())*0.0001);
+        this->setY(this->getY() + this->getKecepatan()*sin(this->getArah())*0.0001);
+        if (abs(this->getX() - pointtujuan.getX()) < 0.1 && abs(this->getY() - pointtujuan.getY()) < 0.1) {
+            pointtujuan.setX(rand()%SCREEN_WIDTH);
+            pointtujuan.setY(rand()%SCREEN_HEIGHT);
+        }
+    } else {        
+
+    }
+}
+
+int Ikan::getBanyakIkan() {
+	return banyakikan;
+}
+
+//virtual method untuk ikan makan
+// void Ikan::makan();
