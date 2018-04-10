a7ffa5048615fe5b511c8b6ea4c03aebf8e0aa0e
AgeMagi <32269979+AgeMagi@users.noreply.github.com>
2018-04-10 21:56:13 +0700

    Update Akuarium.cpp

--- Akuarium.cpp
+++ Akuarium.cpp
@@ -30,23 +30,126 @@ int Akuarium::getLebar() const {
 }
 
 //Mengembalikan list ikan
-List<Ikan*> Akuarium::getListIkan() const {
+List<Ikan*>& Akuarium::getListIkan(){
 	return ikan;
 }
 
-// //Mengembalikan list makanan ikan
-// List<MakananIkan> Akuarium::getListMakananIkan() const {
-// 	return makananikan;
-// }
+List<MakananIkan>& Akuarium::getListMakananIkan(){
+	return makananikan;
+}
 
-// //Mengembalikan list koin
-// List<Koin> Akuarium::getListKoin() const {
-// 	return koin;
-// }
+//Mengembalikan list koin
+List<Koin>& Akuarium::getListKoin(){
+	return koin;
+}
 
-// //prosedur untuk mengupdate isi dari aquarium
-// void Akuarium::updateIsi();
-// //prosedur untuk menggambarkan petak akuarium
-// void Akuarium::gambarAkuarium();
-// //prosedur untuk memunculkan benda akuarium secara random
-// void Akuarium::spawnItem();
+Siput Akuarium::getSiput() const {
+	return siput;
+}
+
+void Akuarium::tambahikan(Ikan* newikan) {
+	ikan.add(newikan);
+}
+
+void Akuarium::tambahmakananikan(MakananIkan newmakananikan) {
+	makananikan.add(newmakananikan);
+} 
+
+void Akuarium::tambahkoin(Koin newkoin) {
+	koin.add(newkoin);
+}
+
+void Akuarium::gerak() {
+	int loop = 0;
+	
+	int dapatkoin = siput.cariKoin(koin);
+            
+    if (dapatkoin != -1) {
+        koin.removeIdx(dapatkoin);
+    }
+	
+	while(loop < ikan.getSize()) {
+        if (ikan.get(loop)->getType() == "Guppy") {
+            if (ikan.get(loop)->mati()) {
+                ikan.removeIdx(loop);
+                continue;
+            }  
+            if (ikan.get(loop)->getLapar()) {
+                int makanandimakan = ikan.get(loop)->cariMakanGuppy(makananikan);
+                if (makanandimakan != -1) {
+                    makananikan.removeIdx(makanandimakan);
+                }
+            } else {
+                ikan.get(loop)->gerak();
+            }
+            if (ikan.get(loop)->keluarkanKoinGuppy()) {
+                if (ikan.get(loop)->getLevel() == 1) {
+                    Koin newkoin(ikan.get(loop)->getX(), ikan.get(loop)->getY(), 10, 10, 1);
+                    koin.add(newkoin);
+                } else if (ikan.get(loop)->getLevel() == 2) {
+                    Koin newkoin(ikan.get(loop)->getX(), ikan.get(loop)->getY(), 10, 20, 2);
+                    koin.add(newkoin);
+                } else {
+                    Koin newkoin(ikan.get(loop)->getX(), ikan.get(loop)->getY(), 10, 50, 3);
+                    koin.add(newkoin);
+                }
+            }              
+        } else {
+            if (ikan.get(loop)->mati()) {
+                ikan.removeIdx(loop);
+                continue;
+            }
+            if (ikan.get(loop)->getLapar()) {
+                int ikandimakan = ikan.get(loop)->cariIkanTerdekat(ikan);
+                if (ikandimakan != -1) {
+                    Koin newkoin(ikan.get(loop)->getX(), ikan.get(loop)->getY(), 10, 100*(ikan.get(ikandimakan)->getLevel()+1), 4);
+                    koin.add(newkoin);
+                    ikan.removeIdx(ikandimakan);
+                }
+            } else {
+                ikan.get(loop)->gerak();
+            }
+        }
+        loop++;
+    }
+
+    for(int i = 0; i < makananikan.getSize(); i++) {
+        makananikan.getRef(i)->gerak();
+        if (abs(makananikan.get(i).getY() - SCREEN_HEIGHT) < 0.1) {
+            makananikan.removeIdx(i);
+        }
+    }
+
+    for(int i = 0; i < koin.getSize(); i++) {
+        koin.getRef(i)->gerak();
+    }
+
+    
+}
+
+void Akuarium::gambarAkuarium(Player habibi) {
+	draw_image("Aquarium6.jpg", SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
+            draw_text("Panah untuk bergerak, r untuk reset, x untuk keluar", 18, 10, 10, 0, 0, 0);
+	for(int i = 0; i < ikan.getSize(); i++) {
+        draw_image(ikan.get(i)->getImage(), ikan.get(i)->getX(), ikan.get(i)->getY()-24);
+    }
+
+    for(int i = 0; i < makananikan.getSize(); i++) {
+        draw_image(makananikan.getRef(i)->getImage(), makananikan.get(i).getX(), makananikan.get(i).getY()-24);
+    }
+
+    for(int i = 0; i < koin.getSize(); i++) {
+        draw_image(koin.getRef(i)->getImage(), koin.get(i).getX(), koin.get(i).getY()-24);
+    }
+
+    if (habibi.getBanyakTelur()==0){
+        draw_image("telor1.png",718,100);
+    }else if (habibi.getBanyakTelur()==1){
+        draw_image("telor2.png",718,100);
+    }else{
+        draw_image("telor3.png",718,100);
+    }
+    draw_image(siput.getImage(), siput.getX(), siput.getY()-24);
+	draw_text("Koin Player : " +  std::to_string(habibi.getKoin()), 18, 10, 50, 0, 0, 0);
+    draw_text("Banyak telur: " + std::to_string(habibi.getBanyakTelur()),18,10,100,0,0,0);
+}
